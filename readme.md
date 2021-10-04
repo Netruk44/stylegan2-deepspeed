f7399c290b87fd967b4581db17a70c6c5a5fdc39
Daniel Perry <daniel.tyler.perry@gmail.com>
2021-10-03 18:36:08 -0700

    Very minor content

---
+++ readme.md
@@ -0,0 +1,9 @@
+# stylegan2-deepspeed
+
+An implementation of stylegan2 using Microsoft's DeepSpeed.
+
+Heavily based on (lucidrain's)[https://github.com/lucidrains/stylegan2-pytorch] implementation.
+
+# References
+
+TODO
---
+++ stylegan2_deepspeed/stylegan2.py
@@ -0,0 +1,70 @@
+# Code mainly from: https://github.com/lucidrains/stylegan2-pytorch/blob/master/stylegan2_pytorch/stylegan2_pytorch.py
+
+from math import floor, log2
+from torch import nn
+
+class Generator(nn.Module):
+    def __init__(self, image_size, latent_dim, network_capacity = 16, transparent = False, attn_layers = [], no_const = False, fmap_max = 512):
+        super().__init__()
+        self.image_size = image_size
+        self.latent_dim = latent_dim
+        self.num_layers = int(log2(image_size) - 1)
+
+        filters = [network_capacity * (2 ** (i + 1)) for i in range(self.num_layers)][::-1]
+
+        set_fmap_max = partial(min, fmap_max)
+        filters = list(map(set_fmap_max, filters))
+        init_channels = filters[0]
+        filters = [init_channels, *filters]
+
+        in_out_pairs = zip(filters[:-1], filters[1:])
+        self.no_const = no_const
+
+        if no_const:
+            self.to_initial_block = nn.ConvTranspose2d(latent_dim, init_channels, 4, 1, 0, bias=False)
+        else:
+            self.initial_block = nn.Parameter(torch.randn((1, init_channels, 4, 4)))
+
+        self.initial_conv = nn.Conv2d(filters[0], filters[0], 3, padding=1)
+        self.blocks = nn.ModuleList([])
+        self.attns = nn.ModuleList([])
+
+        for ind, (in_chan, out_chan) in enumerate(in_out_pairs):
+            not_first = ind != 0
+            not_last = ind != (self.num_layers - 1)
+            num_layer = self.num_layers - ind
+
+            attn_fn = attn_and_ff(in_chan) if num_layer in attn_layers else None
+
+            self.attns.append(attn_fn)
+
+            block = GeneratorBlock(
+                latent_dim,
+                in_chan,
+                out_chan,
+                upsample = not_first,
+                upsample_rgb = not_last,
+                rgba = transparent
+            )
+            self.blocks.append(block)
+
+    def forward(self, styles, input_noise):
+        batch_size = styles.shape[0]
+        image_size = self.image_size
+
+        if self.no_const:
+            avg_style = styles.mean(dim=1)[:, :, None, None]
+            x = self.to_initial_block(avg_style)
+        else:
+            x = self.initial_block.expand(batch_size, -1, -1, -1)
+
+        rgb = None
+        styles = styles.transpose(0, 1)
+        x = self.initial_conv(x)
+
+        for style, block, attn in zip(styles, self.blocks, self.attns):
+            if exists(attn):
+                x = attn(x)
+            x, rgb = block(x, rgb, style, input_noise)
+
+        return rgb
